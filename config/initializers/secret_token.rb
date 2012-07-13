8290abf3ec3c022c9391fef1222d96863dace477
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-07-13 15:53:21 -0700

    Initial rails/devise setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Littlebirdy::Application.config.secret_token = 'c16c79c26f1aa5c63e23fad3913ac210b2a98cda5979b3885f883accb9acd037e0884de3dbff2841417a09e0dee7e147ffb2d08871ea22bfd55f5b7f13f3b1c9'
